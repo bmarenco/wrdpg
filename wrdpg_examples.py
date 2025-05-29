27552c6e37e2120d3d5e340790259f272329e67f
Bernardo Marenco <bmarenco@fing.edu.uy>
2025-05-29 15:04:38 -0300

    Added limiting gaussians to Figures 3 and 4

--- wrdpg_examples.py
+++ wrdpg_examples.py
@@ -6,7 +6,7 @@ Created on Jul 28, 2023
 
 from graspologic.embed import AdjacencySpectralEmbed as ASE
 from graspologic.simulations import er_np, sbm
-from scipy.stats import chi2
+from scipy.stats import chi2, norm
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,34 +24,34 @@ plt.rcParams['axes.grid'] = True
 #np.random.seed(42)
 
 ######### ER + N(1, 1) for all nodes ###########
-n = 1000
+N = 1000
 p = 0.5
 mu = 1
 sigma = 0.1
 sigma_sq = np.square(sigma)
 
-max_k = 6
+K = 6
 
 # Normal distribution moments
-moments_seq = normal_moments(mu,sigma,max_k)
+moments_normal = normal_moments(mu,sigma,2*(K+1))
     
 X_seq = []
 
 # Compute theoretical latent positions
-for moment in moments_seq[1:]:
+for moment in moments_normal[1:K+1]:
     X_seq.append(np.sqrt(p*moment))
     
 w_norm = np.random.normal
 w_norm_args = dict(loc = mu, scale = sigma)
 
-er_graph = er_np(n, p, wt=w_norm, wtargs=w_norm_args)
+er_graph = er_np(N, p, wt=w_norm, wtargs=w_norm_args)
 
 ase = ASE(n_components=1, diag_aug=True, algorithm='truncated')
 
 Xhats = []
 
 # Embed A^(k)
-for k in np.arange(1,max_k+1):
+for k in np.arange(1,K+1):
     Xhat = ase.fit_transform(np.power(er_graph,k))
     if np.mean(Xhat) < 0:
         Xhat = -Xhat 
@@ -64,10 +64,16 @@ for idx, ax in enumerate(axs):
     ax.hist(Xhats[idx], bins=20, density=True, rwidth=0.8)
     ax.axvline(X_seq[idx], color = 'maroon', linestyle = '--')
     ax.set_title(f'$\hat{{\mathbf{{X}}}}[{idx+1}]$')
-
+    # Plot limiting gaussians
+    sigma_sq_lim = (p*moments_normal[2*(idx+1)] - np.square(p*moments_normal[idx+1]))/(p*moments_normal[idx+1])
+    sigma_sq_lim = sigma_sq_lim/N
+    sigma_lim = np.sqrt(sigma_sq_lim)
+    x = np.linspace(X_seq[idx] - 4*sigma_lim, X_seq[idx] + 4*sigma_lim, 100)
+    ax.plot(x, norm.pdf(x, X_seq[idx], sigma_lim),linestyle='dashed', color='black')
+    
 
 ######### 2-block SBM + N(1, 1) for all nodes ###########
-max_k = 6
+K = 6
 N = 1000
 ratio = 0.7
 n = [int(N*ratio),int(N*(1-ratio))]
@@ -85,12 +91,12 @@ P = [[p_c1, q],
 
 
 # Normal distribution moments
-moments_seq = normal_moments(mu,sigma,max_k)
+moments_normal = normal_moments(mu,sigma,2*(K+1))
 
 X_seq = []
 
 # Compute theoretical latent positions
-for moment in moments_seq[1:]:
+for moment in moments_normal[1:K+1]:
     X_c1 = np.array([np.sqrt(p_c1*moment),0])
     X_c2 = np.array([q*np.sqrt(moment/p_c1),np.sqrt(moment*(p_c2-np.square(q)/p_c1))])
     X_seq.append(np.vstack((X_c1,X_c2)))
@@ -102,7 +108,7 @@ ase = ASE(n_components=2, diag_aug=True, algorithm='truncated')
 
 Xhats_sbm = []
 
-for k in np.arange(1,max_k+1):
+for k in np.arange(1,K+1):
     # Embed A^(k)
     Xhat = ase.fit_transform(np.power(sbm_graph,k))
     # Align with theoretical latent positions
@@ -112,21 +118,68 @@ for k in np.arange(1,max_k+1):
     rotated_Xhat = align_Xs(Xhat, X)
     Xhats_sbm.append(rotated_Xhat)
 
+color_list = ['maroon', 'royalblue']
+
 fig, axs = plt.subplots(figsize=(10,16),nrows=2, ncols=3, layout='constrained')
 axs = axs.flatten() 
-colors = n[0]*['maroon'] + n[1]*['royalblue']
+colors = n[0]*[color_list[0]] + n[1]*[color_list[1]]
 
 for idx, ax in enumerate(axs):
     ax.scatter(Xhats_sbm[idx][:,0],Xhats_sbm[idx][:,1],c=colors,alpha=0.1)
     ax.scatter(X_seq[idx][:,0],X_seq[idx][:,1],c='black',marker='x')
     ax.set_title(f'$\hat{{\mathbf{{X}}}}[{idx+1}]$')
 
+# Plot 95% confidence levels for limiting gaussians
+conf_level = 0.95
+
+# Parametrize the unit circle
+theta = np.linspace(0, 2 * np.pi, 200)
+circle = np.stack((np.cos(theta), np.sin(theta)))  # shape (2, N)
+
+# Probabilities of belonging to each sbm block
+pi_1 = ratio
+pi_2 = 1-pi_1
+
+# Compute the squared Mahalanobis radius (quantile of chi-squared with 2 dof)
+r2 = chi2.ppf(conf_level, df=2)
+r = np.sqrt(r2)
+
+for idx,X in enumerate(X_seq):
+    means = [X_seq[idx][0,:],X_seq[idx][1,:]]
+    
+    outer_block1 = np.outer(means[0],means[0])
+    outer_block2 = np.outer(means[1],means[1])
+
+    delta_k = pi_1*outer_block1 + pi_2*outer_block2
+    delta_inv = np.linalg.inv(delta_k)
+
+    E_k_normal = moments_normal[idx+1]
+    E_2k_normal = moments_normal[2*(idx+1)]
+        
+    cov_1 = pi_1*outer_block1*(p_c1*E_2k_normal-np.square(p_c1*E_k_normal)) + pi_2*outer_block2*(q*E_2k_normal-np.square(q*E_k_normal))
+    cov_1 = delta_inv@cov_1@delta_inv
+        
+    cov_2 = pi_1*outer_block1*(q*E_2k_normal-np.square(q*E_k_normal)) + pi_2*outer_block2*(p_c2*E_2k_normal-np.square(p_c2*E_k_normal)) 
+    cov_2 = delta_inv@cov_2@delta_inv
+            
+    cov_1 = cov_1/N
+    cov_2 = cov_2/N
+    covs = [cov_1,cov_2]
+    
+    for comm, (mean,cov) in enumerate(zip(means,covs)):
+        # Cholesky decomposition for transforming the unit circle
+        L = np.linalg.cholesky(cov)
+        ellipsoid = mean[:, None] + r * L @ circle
+        
+        axs[idx].plot(ellipsoid[0], ellipsoid[1], color='black', linestyle='dashed')
+
 
 ######### 2-block SBM + N(5, 0.1) or Poisson(5) ###########
 mu = 5
 sigma = 0.1
 sigma_sq = np.square(sigma)
-lam = mu+0.1
+lam = mu + 0.1
+
 N = 2000
 ratio = 0.5
 n = [int(N*ratio),int(N*(1-ratio))]
@@ -147,23 +200,17 @@ wtargs = [[w_norm_args, w_norm_args],
 
 sbm_graph = sbm(n=n, p=P, wt=wt, wtargs=wtargs)
 
-
-max_k = 3
-
+K = 3
 
 # Normal distribution moments
-moments_normal = np.empty((max_k+1,))
-moments_normal[0] = 1
-moments_normal[1] = mu
-for k in range(2, max_k+1):
-    moments_normal[k] = moments_normal[1]*moments_normal[k-1]+(k-1)*sigma_sq*moments_normal[k-2]
+moments_normal = normal_moments(mu, sigma, 2*(K+1))
 # Poisson distribution moments
-moments_poisson = [1, lam, np.square(lam)+lam, np.power(lam,3)+3*np.square(lam)+lam]
+moments_poisson = poisson_moments(lam, 2*(K+1))
 
 X_seq = []
 
 # Compute theoretical latent positions
-for moment_normal, moment_poisson in zip(moments_normal[1:],moments_poisson[1:]):
+for moment_normal, moment_poisson in zip(moments_normal[1:K+1],moments_poisson[1:K+1]):
     X_c1 = np.array([np.sqrt(p*moment_normal),0])
     X_c2 = np.array([np.sqrt(p*moment_normal),np.sqrt(p*(moment_poisson-moment_normal))])
     X_seq.append(np.vstack((X_c1,X_c2)))
@@ -172,7 +219,7 @@ Xhats_sbm = []
 
 ase = ASE(n_components=2, diag_aug=True, algorithm='truncated')
 
-for k in np.arange(1,max_k+1):
+for k in np.arange(1,K+1):
     # Embed A^(k)
     Xhat = ase.fit_transform(np.power(sbm_graph,k))
     # Align with theoretical latent positions
@@ -182,7 +229,6 @@ for k in np.arange(1,max_k+1):
     rotated_Xhat = align_Xs(Xhat, X)
     Xhats_sbm.append(rotated_Xhat)
 
-color_list = ['maroon', 'royalblue']
 fig, axs = plt.subplots(figsize=(18,6),nrows=1, ncols=3, layout='constrained')
 axs = axs.flatten() 
 colors = n[0]*[color_list[0]] + n[1]*[color_list[1]]
@@ -192,21 +238,10 @@ for idx, ax in enumerate(axs):
     ax.scatter(X_seq[idx][:,0],X_seq[idx][:,1],c='black',marker='x')
     ax.set_title(f'$\hat{{\mathbf{{X}}}}[{idx+1}]$')
 
-# Plot 95% confidence levels for limiting gaussians
-conf_level = 0.95
-
-# Parametrize the unit circle
-theta = np.linspace(0, 2 * np.pi, 200)
-circle = np.stack((np.cos(theta), np.sin(theta)))  # shape (2, N)
 
 # Probabilities of belonging to each sbm block
 pi_1 = ratio
 pi_2 = 1-pi_1
-
-
-# Compute the squared Mahalanobis radius (quantile of chi-squared with 2 dof)
-r2 = chi2.ppf(conf_level, df=2)
-r = np.sqrt(r2)
 
 for idx,X in enumerate(X_seq):
     means = [X_seq[idx][0,:],X_seq[idx][1,:]]
@@ -214,39 +249,26 @@ for idx,X in enumerate(X_seq):
     outer_block1 = np.outer(means[0],means[0])
     outer_block2 = np.outer(means[1],means[1])
 
-    delta_k = p*(pi_1*outer_block1 + pi_2*outer_block2)
+    delta_k = pi_1*outer_block1 + pi_2*outer_block2
     delta_inv = np.linalg.inv(delta_k)
     
-    if idx==0:
-        cov1 = p*delta_inv@(pi_1*outer_block1+pi_2*outer_block2)@delta_inv
-        mu_hat = p*moments_normal[1]
-        cov1 = cov1*(p*mu_hat**2*(1-p)+sigma_sq)
-        
-        lam_hat = p*moments_poisson[1]
-        
-        cov2 = p*delta_inv@(pi_1*(p*mu_hat**2*(1-p)+sigma_sq)*outer_block1+
-                            pi_2*p*lam_hat*(lam_hat*(1-p)+1)*outer_block2)@delta_inv
-                            
-    else:
-        E_k_normal = moments_normal[idx+1]
-        E_2k_normal = normal_moments(mu, sigma, 2*(idx+1))[-1]
-        cov1 = delta_inv@(pi_1*outer_block1+pi_2*outer_block2)@delta_inv
-        
-        cov1 = p*(p*E_2k_normal-np.square(p*E_k_normal))*cov1 
-        
-        E_k_poisson = moments_poisson[idx+1]
-        E_2k_poisson = poisson_moments(lam, 2*(idx+1))[-1]
-        
-        cov2 = pi_1*(p*E_2k_normal-np.square(p*E_k_normal))*outer_block1
-        cov2 += pi_2*(p*E_2k_poisson-np.square(p*E_k_poisson))*outer_block2
-        
-        cov2 = p*delta_inv@cov2@delta_inv
+    E_k_normal = moments_normal[idx+1]
+    E_2k_normal = moments_normal[2*(idx+1)]
+    
+    cov1 =(p*E_2k_normal-np.square(p*E_k_normal))*(pi_1*outer_block1+pi_2*outer_block2)
+    
+    cov1 = delta_inv@cov1@delta_inv 
+    
+    E_k_poisson = moments_poisson[idx+1]
+    E_2k_poisson = moments_poisson[2*(idx+1)]
+    
+    cov2 = pi_1*(p*E_2k_normal-np.square(p*E_k_normal))*outer_block1
+    cov2 += pi_2*(p*E_2k_poisson-np.square(p*E_k_poisson))*outer_block2
+    
+    cov2 = delta_inv@cov2@delta_inv
     
     cov1 = cov1/N
     cov2 = cov2/N
-    
-    cov1_sample = np.cov(Xhats_sbm[idx][:n[0],:].T)
-    cov2_sample = np.cov(Xhats_sbm[idx][n[0]:,:].T)
     
     covs = [cov1,cov2]
     
@@ -255,6 +277,6 @@ for idx,X in enumerate(X_seq):
         L = np.linalg.cholesky(cov)
         ellipsoid = mean[:, None] + r * L @ circle
         
-        axs[idx].plot(ellipsoid[0], ellipsoid[1], color=color_list[comm], linestyle='dashed')
+        axs[idx].plot(ellipsoid[0], ellipsoid[1], color='black', linestyle='dashed')
         
 plt.show()
